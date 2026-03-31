be1f4546e382b4f9fa6f96c19c62cc7501824bd1
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-03-31 23:56:25 +0400

    Add busboy module to req stream.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const PORT = 3000;
+const busboy = require('busboy');
+
 const app = express();
+const PORT = 3000;
 
 app.post('/upload', (req, res) => {
   console.log('POST /upload hit');
+
+  const bb = busboy({ headers: req.headers });
+  bb.on('file', (name, file, info) => {
+    file.resume();
+  });
+
+  req.pipe(bb); // pipe request into busboy
+
   res.end('upload endpoint');
 });
 
