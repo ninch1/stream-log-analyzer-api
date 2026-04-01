f2880b5f6fceb50eccbecb0759d13c711417dea3
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-04-01 22:26:27 +0400

    Better response design. Add basic error handling.

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const PORT = 3000;
 function sortLogsWrapper(response) {
   return function sortLogs(chunk, enc, callback) {
     const textType = chunk.toString().split(' ')[0];
-    if (response[textType]) response[textType]++;
-    else response[textType] = 1;
+    if (response.summary[textType]) response.summary[textType]++;
+    else response.summary[textType] = 1;
     callback();
   };
 }
@@ -18,16 +18,29 @@ function sortLogsWrapper(response) {
 app.post('/upload', (req, res) => {
   console.log('POST /upload hit');
 
-  let response = {};
+  let response = { success: true, summary: {} };
 
   const bb = busboy({ headers: req.headers });
   bb.on('file', (name, file, info) => {
     file.pipe(split2()).pipe(through2(sortLogsWrapper(response)));
+    file.on('error', (err) => {
+      console.log(err);
+      response.success = false;
+    });
   });
 
   req.pipe(bb); // pipe request into busboy
+  bb.on('error', (err) => {
+    response.success = false;
+  });
   bb.on('finish', () => {
-    res.json(response);
+    if (response.success) {
+      res.json(response);
+    } else {
+      console.log(err);
+      response.error = 'An unexpected error';
+      res.json(response);
+    }
   });
 });
 
