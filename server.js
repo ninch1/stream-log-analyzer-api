14501d2e081c7a971b20dc3897be80c9be4cb729
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-04-01 21:13:45 +0400

    Add split2, through2 modules. Implement basic log counter.

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const busboy = require('busboy');
+const split2 = require('split2');
+const through2 = require('through2');
 
 const app = express();
 const PORT = 3000;
 
+function sortLogsWrapper(response) {
+  return function sortLogs(chunk, enc, callback) {
+    const textType = chunk.toString().split(' ')[0];
+    if (response[textType]) response[textType]++;
+    else response[textType] = 1;
+    callback();
+  };
+}
+
 app.post('/upload', (req, res) => {
   console.log('POST /upload hit');
 
+  let response = {};
+
   const bb = busboy({ headers: req.headers });
   bb.on('file', (name, file, info) => {
-    file.resume();
+    file.pipe(split2()).pipe(through2(sortLogsWrapper(response)));
   });
 
   req.pipe(bb); // pipe request into busboy
-
-  res.end('upload endpoint');
+  bb.on('finish', () => {
+    res.json(response);
+  });
 });
 
 app.listen(PORT, () => {
