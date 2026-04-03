e3118211e9f8ec0904be59bb4279096f3e5559aa
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-04-04 00:46:53 +0400

    Add gzip support and improve stream pipeline handling.

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,6 +1,7 @@
 const busboy = require('busboy');
 const split2 = require('split2');
 const through2 = require('through2');
+const zlib = require('zlib');
 const sortLogsWrapper = require('../utils/sortLogs');
 const ErrorResponse = require('../utils/ErrorResponse');
 
@@ -40,13 +41,28 @@ module.exports = (req, res, next) => {
   bb.on('file', (name, file, info) => {
     hasFile = true;
 
+    const filename = info.filename.split('.');
+    const isGzip = filename[filename.length - 1].toUpperCase() === 'GZ';
+
     // busboy stream
     file.on('error', (err) => {
       hasError = true;
       return next(new ErrorResponse('File error', 500));
     });
 
-    const analyzer = file // pipes for counting logs
+    let mainStream = file;
+    if (isGzip) {
+      const gunzip = zlib.createGunzip();
+
+      gunzip.on('error', (err) => {
+        hasError = true;
+        return next(new ErrorResponse('Unzipping error', 500));
+      });
+
+      mainStream = file.pipe(gunzip);
+    }
+
+    const analyzer = mainStream // pipes for counting logs
       .pipe(split2())
       .pipe(through2(sortLogsWrapper(summary, level, allowedLevels)));
 
@@ -54,23 +70,26 @@ module.exports = (req, res, next) => {
       hasError = true;
       return next(new ErrorResponse('File error', 500));
     });
+
+    analyzer.on('finish', () => {
+      // sends response after counting whole file
+      if (!hasError)
+        res.json({
+          success: true,
+          summary,
+        });
+    });
   });
 
   bb.on('error', (err) => {
     hasError = true;
     return next(new ErrorResponse('File error', 500));
   });
-  bb.on('finish', () => {
-    if (!hasFile && !hasError) {
+
+  bb.on('end', () => {
+    if (!hasFile) {
       return next(new ErrorResponse('No file uploaded', 400));
     }
-
-    // sends response after counting whole file
-    if (!hasError)
-      res.json({
-        success: true,
-        summary,
-      });
   });
 
   req.pipe(bb); // pipe request into busboy
